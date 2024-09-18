a9533af9509d5a5eebf975b16b184b2a8ecef1a6
Pravin <pravingaikwad549@gmail.com>
2024-09-18 16:15:37 +0530

    updated the sh

--- first.sh
+++ first.sh
@@ -2,7 +2,7 @@ ls
 echo "runing .sh file"
 docker --version
 docker build -t docker-app .
-docker run -d -p 5000:5000 --name my-flask-app docker-app
+docker run -d -p 9000:9000 --name my-flask-app docker-app
 curl http://localhost:5000
 docker stop my-flask-app
 docker rm my-flask-app
